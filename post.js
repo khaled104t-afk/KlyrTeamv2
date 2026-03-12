959c210f0719eef5720d3aeea3f642124de81bb9
khaled104t-afk <khaled104t@gmail.com>
2026-03-12 22:59:10 +0300

    Delete post.js

--- post.js
+++
@@ -1,7 +0,0 @@
-const http=require('http');
-const data=JSON.stringify({author:'Tester',title:'Hello',content:'This is a test.'});
-const req=http.request({hostname:'localhost',port:3000,path:'/forum/posts',method:'POST',headers:{'Content-Type':'application/json','Content-Length':Buffer.byteLength(data)}},res=>{
-  res.on('data',d=>process.stdout.write(d));
-});
-req.write(data);
-req.end();
